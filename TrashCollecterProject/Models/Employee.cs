731f668a82d4f9bd079b28ee02fccd9b50a8666d
FOUNG <larrymoua24@yahoo.com>
2019-03-04 08:33:24 -0600

    Created parent class User, also added child classes Employee and Customer

---
+++ TrashCollecterProject/Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TrashCollecterProject.Models
+{
+    public class Employee : User
+    {
+        public int EmployeeId { get; set; }
+    }
+}
---
+++ TrashCollecterProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TrashCollecterProject.Startup))]
+namespace TrashCollecterProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
